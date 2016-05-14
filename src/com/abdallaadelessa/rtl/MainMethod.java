9068bc1508c7329de150c0c0087cdcf32b64c8f1
abdalla adel <abdalla.essa92@gmail.com>
2016-05-14 23:13:32 +0200

    clean code

--- src/com/abdallaadelessa/rtl/MainMethod.java
+++ src/com/abdallaadelessa/rtl/MainMethod.java
@@ -8,14 +8,14 @@ public class MainMethod {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String dir = "D:\\Workspace\\Projects\\LinkDev\\SoutAlKhaleeg\\app\\src\\main\\res\\layout";
-		RTLConvertor.convertXmlFiles(new File(dir).listFiles());
+//		String dir = "D:\\Workspace\\Projects\\LinkDev\\SoutAlKhaleeg\\app\\src\\main\\res\\layout";
+//		RTLConvertor.convertXmlFiles(new File(dir).listFiles());
 		
 //		String path = "C:\\Users\\Abdalla\\Desktop\\layout.xml";
 //		RTLConvertor.convertXmlFile(new File(path));
 
-		// System.out.println(RTLConvertor.convertXmlLine("android:layout_alignTop=\"@+id/tvAddress\""
-		// + "  android:gravity=\"top|left|start\""));
+// 		System.out.println(RTLConvertor.convertXmlLine("android:layout_alignTop=\"@+id/tvAddress\""
+// 		+ "  android:gravity=\"top|left|start\""));
 	}
 
 }
